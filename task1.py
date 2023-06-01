958b467d3d02239855d8e1de9e4ecf6b9e5be3b7
Silviaicm <silviaisabelc345@deltalearns.ca>
2023-06-01 00:34:30 -0700

    try, not sure if its right

--- task1.py
+++ task1.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+
 """
 Task 1
 Read the map1.txt file and convert to a map that you can navigate a
@@ -9,7 +10,31 @@ w.geometry("925x475")
 w.attributes('-topmost',True)
 c = tk.Canvas(height=475,width=900,bg="#ffdddd")
 c.pack()
-f = open('map1.txt')
 
+count = 0
+county = 0
+countGrid = 0  
+i = 0
 
+walls = []
+
+def place():
+    global count,county,countGrid,i,w,walls,c
+    f = open('map1.txt')
+    data = f.read()
+    data = list(data)
+    if data[i] == "*":
+        walls.append( c.create_rectangle(0 + (25 * count),0 + (25 * county),25 + (25 * count),25 + (25 * county), fill = "black") )
+    count +=1
+    countGrid += 1
+    if countGrid == 30 or data[i] == "\n":
+        county += 1
+        count = 0
+        countGrid = 0
+    i += 1    
+    timer = 0 if data[i] != "*" else 20
+    if i < len(data):
+        w.after(timer,place)
+
+w.after(200,place)
 w.mainloop()
